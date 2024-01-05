8ed5d720e8b067b8d263ba47d9205ad200395f80
joker-aleks <aleks.valushko@yandex.ru>
2024-01-05 09:35:24 +0300

    branch_1: app file and styles modified

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import styles from './App.module.scss';
 import AppRouter from './components/AppRouter';
 import Header from './components/Header';
+import { useAppSelector } from './hooks/redux';
 
 const App = () => {
+	const { user } = useAppSelector((state) => state.userReducer);
 	return (
 		<div className={styles.app}>
-			<Header />
-			<AppRouter />
+			{user.isAuth ? '' : <Header />}
+			<AppRouter user={user} />
 		</div>
 	);
 };
