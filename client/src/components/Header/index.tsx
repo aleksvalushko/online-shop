f2b7a09a2e5b72a5c6aa6c5a26c340918ad68974
joker-aleks <aleks.valushko@yandex.ru>
2024-01-05 09:37:19 +0300

    branch_1: header component and styles modified

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,56 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss';
-import logo from '../../assets/images/logo.png';
 import useLanguage from '../../context/language/useLanguage';
-import { OutlinedHeartIcon, LocationIcon, ProfileIcon, ShoppingCartIcon } from '../../assets/icons/Icons';
 import { Button, Dropdown, Space } from 'antd';
-import type {MenuProps} from 'antd';
+import type { MenuProps } from 'antd';
 import { EN, RU } from '../../constants';
+import { MenuOutlined } from '@ant-design/icons';
+import Menu from '../Menu';
+import BurgerMenu from '../BurgerMenu';
+import logo from '../../assets/images/logo.png';
 
 const items: MenuProps['items'] = [
 	{ key: RU, label: 'Русский' },
 	{ key: EN, label: 'Английский' }
 ];
 
-function Header() {
-
+const Header = () => {
+	const [isBurgerMenuShown, setIsBurgerMenuShown] = useState(false);
 	const { currentLanguage, setCurrentLanguage, translate } = useLanguage();
 
-	const handleMenuClick: MenuProps['onClick'] = (e) => {
+	const handleLanguageMenuClick: MenuProps['onClick'] = (e) => {
 		setCurrentLanguage && setCurrentLanguage(e.key);
+	};
+	const handleBurgerMenuClick = () => {
+		setIsBurgerMenuShown(!isBurgerMenuShown);
 	};
 
 	const menuProps = {
 		items,
-		onClick: handleMenuClick,
+		onClick: handleLanguageMenuClick
 	};
 
 	return (
 		<div className={styles.header}>
-			<div className={styles.info}>
-				<div className={styles.stocks}>{translate && translate('header.stocks')}</div>
-				<div className={styles.paymentAndDelivery}>{translate && translate('header.paymentAndDelivery')}</div>
-				<div className={styles.logo}><img src={logo} alt='logo' /></div>
-				<div className={styles.address}>
-					<LocationIcon style={{fontSize: '30px'}} />
-					{translate && translate('header.address')}
-				</div>
-				<div className={styles.userButtons}>
-					<Dropdown menu={menuProps} className={styles.languageMenu}>
-						<Button>
-							<Space>
-								{currentLanguage}
-							</Space>
-						</Button>
-					</Dropdown>
-					<div className={styles.liked}><OutlinedHeartIcon /></div>
-					<div className={styles.profile}><ProfileIcon /></div>
-					<div className={styles.shoppingCart}><ShoppingCartIcon /></div>
-				</div>
+			<div className={styles.burgerMenuButtonWrapper}>
+				<Button
+					icon={<MenuOutlined />}
+					size='large'
+					className={styles.burgerMenuButton}
+					onClick={handleBurgerMenuClick}></Button>
 			</div>
-			<div className={styles.menu}></div>
+			<div className={styles.logo}>
+				<img src={logo} alt='logo' />
+			</div>
+			<Dropdown menu={menuProps} className={styles.languageMenu} placement={'bottomRight'}>
+				<Button>
+					<Space>{currentLanguage}</Space>
+				</Button>
+			</Dropdown>
+
+			{isBurgerMenuShown && (
+				<div className={styles.burgerMenuWrapper}>
+					<BurgerMenu translate={translate} handleBurgerMenuClick={handleBurgerMenuClick} />
+				</div>
+			)}
+
+			<div className={styles.info}>
+				<Menu menuProps={menuProps} currentLanguage={currentLanguage} translate={translate} />
+			</div>
 		</div>
 	);
-}
+};
 
 export default Header;
