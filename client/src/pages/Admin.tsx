40438d9c48d4390729312875e8aa3e49a32e3d0c
joker-aleks <aleks.valushko@yandex.ru>
2024-01-05 09:33:09 +0300

    branch_1: some pages added, some modified

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Layout, Menu } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import styles from './styles.module.scss';
+import useLanguage from '../context/language/useLanguage';
+
+const { Header, Sider, Content } = Layout;
 
 const Admin = () => {
+	const { translate } = useLanguage();
+
 	return (
-		<div>
-			Admin
-		</div>
+		<Layout className={styles.adminPanelPage}>
+			<Header className={styles.header}>{translate ? translate('admin.shopName') : ''}</Header>
+			<Layout>
+				<Sider trigger={null} className={styles.sider}>
+					<Menu
+						theme='light'
+						mode='inline'
+						defaultSelectedKeys={['1']}
+						items={[
+							{
+								key: '1',
+								icon: <UserOutlined />,
+								label: 'nav 1111111111111111111111111111'
+							},
+							{
+								key: '2',
+								icon: <VideoCameraOutlined />,
+								label: 'nav 2'
+							},
+							{
+								key: '3',
+								icon: <UploadOutlined />,
+								label: 'nav 3'
+							}
+						]}
+					/>
+				</Sider>
+				<Layout className={styles.content}>
+					<Content
+						style={{
+							padding: 24,
+							minHeight: 280,
+							background: 'white'
+						}}>
+						Content
+					</Content>
+				</Layout>
+			</Layout>
+		</Layout>
 	);
 };
 
---
+++ client/src/pages/StocksPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StocksPage = () => {
+	return <div>Stocks</div>;
+};
+
+export default StocksPage;
